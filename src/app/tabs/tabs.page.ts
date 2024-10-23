825c81966e08daf52552e1d48797b292470ff74d
3bouda <abdelaliiheb@gmail.com>
2024-10-23 13:42:33 +0100

    menu

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../auth/service/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage implements OnInit{
+ user:any;
+  constructor(private authService: AuthService,private router: Router) {}
 
-  constructor() {}
-
+   ngOnInit() {
+    this.authService.getCurrentUser()
+      .subscribe(user => {
+        this.user = user?.displayName
+      })
+  }
+  logOut(){
+    this.authService.logout().then(r =>
+      this.router.navigate(['/login'])
+    );
+  }
 }
